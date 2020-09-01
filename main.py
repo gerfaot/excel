e35991e0264a7c552407d0ab07994fc2e6401a9e
gerfaot <soldvds@gmail.com>
2020-09-01 05:45:11 +0300

    Second

--- main.py
+++ main.py
@@ -2,20 +2,38 @@ import xlsxwriter
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import openpyxl
-
-excel_data_df = pd.read_excel('Образец.xlsx', sheet_name='Лист1')
+excel_data_df = pd.read_excel('ГРАФИК.xlsx', sheet_name='Мультиконтейнеры')
 container = excel_data_df['№ контейнера'].tolist()
 i = 1
-workbook = xlsxwriter.Workbook('hello.xlsx')
+workbook = xlsxwriter.Workbook('next.xlsx')
 worksheet = workbook.add_worksheet()
+exep = 'problem'
 for item in container:
-    response = requests.get('http://www.cma-cgm.com/ebusiness/tracking/search?SearchBy=Container&Reference=' + item)
-    soup = BeautifulSoup(response.text, "lxml")
-    worksheet.write(0, i, soup.findAll("td", class_="is-header js-openrow")[-1].text)
-    i += 1
-    print(soup.findAll("td", class_="is-header js-openrow")[-1].text)
+    try:
+        response = requests.get('http://www.cma-cgm.com/ebusiness/tracking/search?SearchBy=Container&Reference=' + item)
+        soup = BeautifulSoup(response.text, "lxml")
+        worksheet.write(i, 4, item)
+        worksheet.write(i, 5, soup.findAll("td", class_="is-header js-openrow")[-1].text.strip())
+        worksheet.write(i, 6, soup.findAll("td", class_="is-headerdata js-openrow")[-1].text.strip())
+        worksheet.write(i, 7, soup.findAll("td", attrs={'data-label': 'Vessel'})[-1].text.strip())
+        print(i)
+        i += 1
+        print(soup.findAll("td", class_="is-header js-openrow")[-1].text.strip())
+        print(soup.findAll("td", class_="is-headerdata js-openrow")[-1].text.strip())
+        print(soup.findAll("td", attrs={'data-label': 'Vessel'})[-1].text.strip())
+    except Exception as ex:
+        print('exeption', ex)
+        worksheet.write(i, 4, item)
+        worksheet.write(i, 5, exep)
+        print(i)
+        i += 1
+        continue
 workbook.close()
+
+
+
+
+
 
 #wb = openpyxl.load_workbook('Образец.xlsx') #Открываем тестовый Excel файл
 #wb.create_sheet('Sheet1') #Создаем лист с названием "Sheet1"
